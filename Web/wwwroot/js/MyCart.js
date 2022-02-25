2fefced74ef436455ca5ba600a59b1e4dc276357
UlfetZakirli <ulfetzakirli@gmail.com>
2022-02-25 15:22:00 +0400

    1

--- Web/wwwroot/js/MyCart.js
+++ Web/wwwroot/js/MyCart.js
@@ -1,4 +1,5 @@
-﻿function setCookie(cname, cvalue, exdays) {
+﻿////JAVAsCRIPT
+function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
